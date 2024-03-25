c20fd23ec5e2500eda3a5f0e309207267e26de86
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-03-25 16:25:40 -0300

    feat: add coffe shop vue project

---
+++ javascript/frameworks/vue/coffe-shop/src/components/Cart.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+    <section>
+        <div class="container">
+            <h2>Cart</h2>
+
+            <ul>
+                <li></li>
+            </ul>
+        </div>
+    </section>
+</template>
+
+<style scoped></style>

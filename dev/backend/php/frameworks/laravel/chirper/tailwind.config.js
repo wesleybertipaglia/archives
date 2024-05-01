8477420d556d8ded14df5c499eccde07e34c89e7
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-05-01 12:30:35 -0300

    feat: add chiper project

---
+++ dev/backend/php/frameworks/laravel/chirper/tailwind.config.js
@@ -0,0 +1,24 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./vendor/**/*.blade.php",
+        "./vendor/**/*.js",
+        "./storage/**/*.blade.php",
+        "./storage/**/*.js",
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+
+    plugins: [forms],
+};
